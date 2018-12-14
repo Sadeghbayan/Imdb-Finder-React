d5dbfc426363d8f635d4b4eda86c7c7fcc5e2b69
Soheil Sadeghbayan <s.sadeghbayan@atitel.com>
2018-12-14 20:26:45 +0330

    set to github pages

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, HashRouter} from 'react-router-dom';
 import App from '../App'
 import SingleMovie from './SingleMovie'
 import Error from './Error'
 
 const Router = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Route path="/" component={App} exact />
             <Route path="/movie/:id" component={SingleMovie}/>
             <Route component={Error} />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Router;
