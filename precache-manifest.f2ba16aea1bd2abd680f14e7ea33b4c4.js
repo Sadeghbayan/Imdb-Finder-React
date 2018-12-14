1fff73374882935f9c9d7c6a153398f71b64bccb
Soheil Sadeghbayan <s.sadeghbayan@atitel.com>
2018-12-14 20:07:10 +0330

    Updates

---
+++ precache-manifest.f2ba16aea1bd2abd680f14e7ea33b4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed67635ffacb6cc65fc",
+    "url": "/Imdb-Finder-React/static/css/main.d9e6d48a.chunk.css"
+  },
+  {
+    "revision": "eed67635ffacb6cc65fc",
+    "url": "/Imdb-Finder-React/static/js/main.eed67635.chunk.js"
+  },
+  {
+    "revision": "0d5f715ecc545d6df179",
+    "url": "/Imdb-Finder-React/static/css/1.cc6160d0.chunk.css"
+  },
+  {
+    "revision": "0d5f715ecc545d6df179",
+    "url": "/Imdb-Finder-React/static/js/1.0d5f715e.chunk.js"
+  },
+  {
+    "revision": "1ab0363cfecef5396944",
+    "url": "/Imdb-Finder-React/static/js/runtime~main.1ab0363c.js"
+  },
+  {
+    "revision": "c7421205d1598c19b78d92da90b08e5b",
+    "url": "/Imdb-Finder-React/static/media/poster.c7421205.jpg"
+  },
+  {
+    "revision": "864ce7706d2e36e23406bfdf72a4fa13",
+    "url": "/Imdb-Finder-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/[your-user-name].github.io/Imdb-Finder-React/precache-manifest.d55508355a89998c05b38fe939b93306.js"
+  "/Imdb-Finder-React/precache-manifest.f2ba16aea1bd2abd680f14e7ea33b4c4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/[your-user-name].github.io/Imdb-Finder-React/index.html", {
+workbox.routing.registerNavigationRoute("/Imdb-Finder-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
