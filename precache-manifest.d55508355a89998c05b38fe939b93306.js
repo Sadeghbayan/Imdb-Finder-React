27ab8e8ee921930c5564986c375f08ddf8ef994b
Soheil Sadeghbayan <s.sadeghbayan@atitel.com>
2018-12-14 20:02:08 +0330

    Updates

---
+++ precache-manifest.d55508355a89998c05b38fe939b93306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd4cf18fa951a935ad5",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/static/css/main.d9599326.chunk.css"
+  },
+  {
+    "revision": "dcd4cf18fa951a935ad5",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/static/js/main.dcd4cf18.chunk.js"
+  },
+  {
+    "revision": "0d5f715ecc545d6df179",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/static/css/1.cc6160d0.chunk.css"
+  },
+  {
+    "revision": "0d5f715ecc545d6df179",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/static/js/1.0d5f715e.chunk.js"
+  },
+  {
+    "revision": "7b68be4b56fd55930610",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/static/js/runtime~main.7b68be4b.js"
+  },
+  {
+    "revision": "c7421205d1598c19b78d92da90b08e5b",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/static/media/poster.c7421205.jpg"
+  },
+  {
+    "revision": "b10c06ae14b2f6c636c748122d8f10a4",
+    "url": "/[your-user-name].github.io/Imdb-Finder-React/index.html"
+  }
+];
